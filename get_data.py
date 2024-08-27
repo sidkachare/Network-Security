50fd431201911b01e4c08e0cdf4edbc05db07759
Siddhesh Kachare <siddheshkachare111@gmail.com>
2024-08-27 15:16:23 +0530

    get data updated

--- get_data.py
+++ get_data.py
@@ -24,19 +24,35 @@ class NetworkDataExtract():
         except Exception as e:
             raise NetworkSecurityException(e, sys)
         
-    def csv_to_json_converter(self):
+    def csv_to_json_converter(self, file_path):
         try:
-            pass
+            data = pd.read_csv(file_path)
+            data.reset_index(drop = True, inplace = True)
+            records = list(json.loads(data.T.to_json()).values())
+            return records
         except Exception as e:
             raise NetworkSecurityException(e, sys)
         
-    def pushing_data_to_mongodb(self):
+    def pushing_data_to_mongodb(self, records, database, collection):
         try:
-            pass
+            self.database = database
+            self.collection = collection
+            self.records = records
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
+            self.database = self.mongo_client[self.database]
+            self.collection = self.database[self.collection]
+            self.collection.insert_many(self.records)
+            return len(self.records)
         except Exception as e:
             raise NetworkSecurityException(e, sys)
         
 if __name__ == '__main__':
-    pass
+    FILE_PATH = './NETWORK_DATA/NetworkData.csv'
+    DATABASE = 'MyDatabase'
+    COLLECTION = 'NetworkData'
+    networkobj = NetworkDataExtract()
+    records = networkobj.csv_to_json_converter(FILE_PATH)
+    noofrecords = networkobj.pushing_data_to_mongodb(records, DATABASE, COLLECTION)
+    print(noofrecords)
 
 
