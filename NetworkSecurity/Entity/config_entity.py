4629fe4035034cc8e03984a1edd1a15c00c4dc2e
Siddhesh Kachare <siddheshkachare111@gmail.com>
2024-08-28 15:14:03 +0530

    config and artifact structure updated

---
+++ NetworkSecurity/Entity/config_entity.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+import os
+from NetworkSecurity.Constant import training_pipeline
+
+print(training_pipeline.ARTIFACT_DIR)
+
+class TrainingPipelineConfig:
+    def __init__(self):
+        pass
+
+class DataIngestionConfig:
+    def __init__(self):
+        pass
+
+class DataValidationConfig:
+    def __init__(self):
+        pass
+
+class DataTransformationConfig:
+    def __init__(self):
+        pass
+
+class ModelTrainerConfig:
+    def __init__(self):
+        pass
+
+class ModelEvaluationConfig:
+    def __init__(self):
+        pass
+
+class ModelPusherConfig:
+    def __init__(self):
+        pass
