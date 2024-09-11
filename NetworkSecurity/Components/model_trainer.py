fba77531558149990b8d8e1c57153f808e10b4ed
Siddhesh Kachare <siddheshkachare111@gmail.com>
2024-09-11 11:21:29 +0530

    model training preparation completed

--- NetworkSecurity/Components/model_trainer.py
+++ NetworkSecurity/Components/model_trainer.py
@@ -1,3 +1,13 @@
+import os
+import sys
+from NetworkSecurity.Exception.exception import NetworkSecurityException
+from NetworkSecurity.logger.logger import logging
+from NetworkSecurity.Entity.artifact_entity import DataTransformationArtifact, ModelTrainerArtifact
+from NetworkSecurity.Entity.config_entity import ModelTrainerConfig
+from xgboost import XGBClassifier
+from NetworkSecurity.utils.ml_utils.model.estimator import NetworkModel
+from NetworkSecurity.utils.main_utils.utils import save_object, load_object
+from NetworkSecurity.utils.main_utils.utils import load_numpy_array_data
 class ModelTrainer:
     def __init__(self):
         pass
